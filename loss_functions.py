fa8da1ea1eee7c5231f3e375c14017c426733f0e
jeroenwijnen98 <j.wijnen@outlook.com>
2022-05-20 12:08:16 +0200

    made bayesian loss function

--- loss_functions.py
+++ loss_functions.py
@@ -2,6 +2,7 @@ from typing import Callable
 
 import torch
 from torch import nn, Tensor
+import torch.distributions.beta as dist_beta
 
 
 class PonderLoss(nn.Module):
@@ -57,6 +58,61 @@ class PonderLoss(nn.Module):
         l_reg = self.beta * self.KL(
             p.transpose(1, 0).log(),
             self.prior[:n_steps].unsqueeze(0).expand(batch_size, n_steps),  # type: ignore
+        )
+
+        return l_rec + l_reg
+
+class PonderBayesianLoss(nn.Module):
+    def __init__(
+        self,
+        task_loss_fn: Callable,
+        beta_prior: tuple,
+        max_ponder_steps: int,
+    ):
+        """
+        Args:
+            # beta: Weight for the regularization loss term.
+            lambda_reg: Parameterizes the (Bernoulli) prior.
+            task_loss_fn: Loss function for the actual task (e.g. MSE or CE).
+            max_ponder_steps
+        """
+        super().__init__()
+        self.task_loss_fn = task_loss_fn
+        # self.beta = beta
+        self.beta_prior = beta_prior
+        self.KL = nn.KLDivLoss(reduction="batchmean")
+
+        self.prior = dist_beta.Beta(beta_prior[0], beta_prior[1])
+
+    def forward(self, preds: Tensor, p: Tensor, lambdas: Tensor, halted_at: Tensor, targets: Tensor):
+        """
+        Args:
+            `preds`: Predictions of shape (ponder_steps, batch_size, logits)
+            `p`: Cumulative probability of reaching and then stopping at each
+                step of shape (step, batch)
+            `halted_at`: Indices of steps where each sample actually stopped of
+                shape (batch)
+            `targets`: Targets of shape (batch_size)
+
+        """
+        n_steps, batch_size, _ = preds.shape
+
+        # Reconstruction term
+        task_losses = self.task_loss_fn(
+            preds.view(
+                -1, preds.size(-1)
+            ),  # View pred steps as individual classifications.
+            targets[
+                (torch.arange(targets.size(0)).repeat(n_steps))
+            ],  # Repeat targets as needed to match.
+            reduction="none",
+        ).view(n_steps, batch_size)
+        l_rec = torch.log(torch.einsum("ij,ij->j", p, task_losses).mean())
+
+        # Regularization term
+        l_reg = self.KL(
+            lambdas.transpose(1, 0).log(),
+            self.prior.rsample(sample_shape=(batch_size, n_steps)),  # type: ignore
         )
 
         return l_rec + l_reg
