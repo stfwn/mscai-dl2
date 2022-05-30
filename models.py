6c347e89bf0a87ffba3e06985294283f8ee2486d
stfwn <stefan@stfwn.com>
2022-05-30 19:37:33 +0200

    Remove unused imports

--- models.py
+++ models.py
@@ -2,8 +2,6 @@ from typing import Literal, Optional
 
 import torch
 import torch.distributions.beta as dist_beta
-import torch.distributions.categorical as dist_categorical
-import torch.distributions.geometric as dist_geometric
 import torch.nn.functional as F
 import torchmetrics
 import torchvision
